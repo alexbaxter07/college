66b3195829e57a614a987949701b3997541e19ea
Alexia <alexiabaxter07@gmail.com>
2024-09-17 12:13:48 +0100

    created: php lessons

---
+++ Lessons/php/1st lesson/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Index</title>
+        <?php
+
+
+        ?>
+
+
+
+    </head>
+
+    <body>
+
+    <?php
+
+        echo"<p id='Steve'>Hello</p>"
+
+    ?>
+
+
+    </body>
+</html>
