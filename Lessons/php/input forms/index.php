d2d4fb95ee4bbeffb22cd03bcd4ad4a2382d2adc
Alexia <alexiabaxter07@gmail.com>
2024-09-18 10:49:37 +0100

    updated: styles

--- Lessons/php/input forms/index.php
+++ Lessons/php/input forms/index.php
@@ -9,50 +9,52 @@
 
     <body>
 
-        <h1>Basic Calculator</h1>
+        <div id="container">
+            <h1>Basic Calculator</h1>
 
-        <form method="post" action="">
+            <form method="post" action="">
 
-            <input type="text" name="num1" placeholder="Enter first number" required>
-            <input type="text" name="num2" placeholder="Enter second number" required>
-            <select name="operator" id="operator">
-                <option value="+">+</option>
-                <option value="-">-</option>
-                <option value="/">/</option>
-                <option value="*">*</option>
-            </select>
-            <input type="submit" value="Calculate">
+                <input type="text" name="num1" placeholder="Enter first number" required>
+                <input type="text" name="num2" placeholder="Enter second number" required>
+                <select name="operator" id="operator">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                </select>
+                <input type="submit" value="Calculate">
 
-        </form>
+            </form>
 
-        <?php
+            <?php
 
-            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-                $num1 = $_POST['num1'];
-                $num2 = $_POST['num2'];
-                $operator = $_POST['operator'];
+                    $num1 = $_POST['num1'];
+                    $num2 = $_POST['num2'];
+                    $operator = $_POST['operator'];
 
-                if (is_numeric($_POST["num1"]) && is_numeric($_POST["num2"])) {
+                    if (is_numeric($_POST["num1"]) && is_numeric($_POST["num2"])) {
 
-                    if ($operator == "+") {
-                        $result = $num1 + $num2;
-                    } else if ($operator == "-") {
-                        $result = $num1 - $num2;
-                    } else if ($operator == "/") {
-                        $result = $num1 / $num2;
-                    } else if ($operator == "*") {
-                        $result = $num1 * $num2;
+                        if ($operator == "+") {
+                            $result = $num1 + $num2;
+                        } else if ($operator == "-") {
+                            $result = $num1 - $num2;
+                        } else if ($operator == "/") {
+                            $result = $num1 / $num2;
+                        } else if ($operator == "*") {
+                            $result = $num1 * $num2;
+                        }
+                        echo $result;
+                    } else{
+                        echo"You haven't entered numbers";
                     }
-                    echo $result;
-                } else{
-                    echo"You haven't entered numbers";
+
                 }
 
-            }
-
-        ?>
-
+            ?>
+        </div>
+    
     </body>
 
 </html>
