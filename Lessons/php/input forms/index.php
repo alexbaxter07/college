5544f7d3c06a9cd4b3e9d8827f3f8b4f61c3860f
Alexia <161457494+alexbaxter07@users.noreply.github.com>
2024-09-19 13:42:38 +0100

    updated: padding and validaition

--- Lessons/php/input forms/index.php
+++ Lessons/php/input forms/index.php
@@ -10,49 +10,65 @@
     <body>
 
         <div id="container">
+
             <h1>Basic Calculator</h1>
 
-            <form method="post" action="">
+            <div id="content">
 
-                <input type="text" name="num1" placeholder="Enter first number" required>
-                <input type="text" name="num2" placeholder="Enter second number" required>
-                <select name="operator" id="operator">
-                    <option value="+">+</option>
-                    <option value="-">-</option>
-                    <option value="/">/</option>
-                    <option value="*">*</option>
-                </select>
-                <input type="submit" value="Calculate">
+                <p>This is a basic calculator. It cannot divide by 0 and an only do addition, subtraction, division and multiplication. </p>
 
-            </form>
+                <form method="post" action="">
 
-            <?php
+                    <input type="text" name="num1" placeholder="Enter first number" required>
+                    <input type="text" name="num2" placeholder="Enter second number" required>
+                    <select name="operator" id="operator">
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                        <option value="/">/</option>
+                        <option value="*">*</option>
+                    </select>
+                    <input type="submit" value="Calculate">
 
-                if($_SERVER["REQUEST_METHOD"] == "POST") {
+                </form>
 
-                    $num1 = $_POST['num1'];
-                    $num2 = $_POST['num2'];
-                    $operator = $_POST['operator'];
+                <div id="calculaition">
 
-                    if (is_numeric($_POST["num1"]) && is_numeric($_POST["num2"])) {
+                    <?php
 
-                        if ($operator == "+") {
-                            $result = $num1 + $num2;
-                        } else if ($operator == "-") {
-                            $result = $num1 - $num2;
-                        } else if ($operator == "/") {
-                            $result = $num1 / $num2;
-                        } else if ($operator == "*") {
-                            $result = $num1 * $num2;
+                        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                            $num1 = $_POST['num1'];
+                            $num2 = $_POST['num2'];
+                            $operator = $_POST['operator'];
+
+                            if (is_numeric($_POST["num1"]) && is_numeric($_POST["num2"])) {
+
+                                if ($operator == "+") {
+                                    $result = $num1 + $num2;
+                                } else if ($operator == "-") {
+                                    $result = $num1 - $num2;
+                                } else if ($operator == "*") {
+                                    $result = $num1 * $num2;
+                                }else if ($operator == "/") {
+                                    if( $num1 <> 0 && $num2 <> 0 ){
+                                        $result = $num1 / $num2;
+                                    }else{
+                                        echo "Please enter a valid number you cannot divide by 0";
+                                        $result = "";
+                                    }
+                                }
+                                echo "Your result is " . $result;
+                            } else{
+                                echo"You haven't your numbers correctly please check they are integers and try again";
+                            }
+
                         }
-                        echo $result;
-                    } else{
-                        echo"You haven't entered numbers";
-                    }
 
-                }
+                    ?>
+                </div>
 
-            ?>
+            </div>
+
         </div>
     
     </body>
