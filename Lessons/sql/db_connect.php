0532c68b980dbea56b9d9e85cc5a8fa071065489
Alexia <alexiabaxter07@gmail.com>
2024-09-24 09:59:29 +0100

    updated: connected database

--- Lessons/sql/db_connect.php
+++ Lessons/sql/db_connect.php
@@ -1,1 +1,16 @@
 <?php
+
+    $servername = "localhost";
+    $username = "membs";
+    $password = "Password";
+    $dname = "membs";
+
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $dname);
+
+    //check connection
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
