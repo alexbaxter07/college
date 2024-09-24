efa289dd38dd788a98f891df1caeadeacd7fb6f4
Alexia <alexiabaxter07@gmail.com>
2024-09-24 11:04:27 +0100

    updated: records

--- Lessons/sql/reg.php
+++ Lessons/sql/reg.php
@@ -1,1 +1,22 @@
 <?php
+
+    include "db_connect.php";
+
+    $Usern = $_POST['Uname'];
+    $pswd = $_POST['Password'];
+    $fname = $_POST['Fname'];
+    $sname = $_POST['Sname'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO mem(Username, Password, Fname, Sname, Email)VAlUES(?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 'sssss', $Usern, $pswd, $fname, $sname, $email);
+    mysqli_stmt_execute($stmt);
+
+    if(mysqli_stmt_error($stmt)){
+        echo"Error: " . mysqli_stmt_error($stmt);
+    }else{
+        echo"Records created successfully";
+    }
+
+?>
