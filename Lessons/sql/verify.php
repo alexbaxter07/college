7f4ddfc0b0fd872c02716bdb40a5adc0b254e931
Alexia <alexiabaxter07@gmail.com>
2024-09-25 10:24:52 +0100

    updated : verificaition for login

--- Lessons/sql/verify.php
+++ Lessons/sql/verify.php
@@ -1,1 +1,20 @@
 <?php
+
+    include 'db_connect.php';
+
+    $Usern = $_POST['Uname'];
+    $pswd = $_POST['Password'];
+    $cpaswd = $_POST['CPassword'];
+
+    $sql = "SELECT * FROM `membs` WHERE UserName = '$Usern'";
+
+    if ($sql == false) {
+        header("refresh:5; url=index.html");
+        echo "User not found. Please Sign Up";
+    }elseif ($pswd <> $cpaswd ){
+        header("refresh:5; url=login.html");
+        echo "Passwords do not match. please try again";
+    }else{
+        echo "You are now logged in";
+    }
+?>
