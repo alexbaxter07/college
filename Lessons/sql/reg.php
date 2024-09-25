36ce96325a63e44826908af08098af4fa5db4b39
Alexia <alexiabaxter07@gmail.com>
2024-09-25 10:34:49 +0100

    update: goes to signup

--- Lessons/sql/reg.php
+++ Lessons/sql/reg.php
@@ -12,22 +12,22 @@
     $hashed_pswd = password_hash($pswd, PASSWORD_DEFAULT);
 
     if($pswd <> $cpaswd ){
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "The passwords did not match, you will be redirected in 5 seconds.";
     }elseif(preg_match("/[a-z]/", $pswd) == false){
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "There are no lowercase letters";
     }elseif(preg_match("/[A-Z]/", $pswd) == false){
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "There are no uppercase letters";
     }elseif(preg_match("/[0-9]/", $pswd) == false) {
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "There are no numbers";
     }elseif(preg_match("/[^A-Za-z0-9]/", $pswd) == false) {
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "There are no special characters";
     }elseif(strlen($pswd) < 8){
-        header("refresh:5; url=index.html");
+        header("refresh:5; url=signup.html");
         echo "Password is less than 8 characters";
     }else{
         $sql = "INSERT INTO mem(Username, Password, Fname, Sname, Email)VAlUES(?, ?, ?, ?, ?)";
