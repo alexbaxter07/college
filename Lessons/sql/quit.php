f2897688b432d15386a90ad49e36f3f9663986da
Alexia <alexiabaxter07@gmail.com>
2024-10-03 11:01:12 +0100

    updated: exit

--- Lessons/sql/quit.php
+++ Lessons/sql/quit.php
@@ -9,4 +9,7 @@
     // destroy the session
     session_destroy();
 
+    header("refresh:5; url=index.html");
+    echo "You have now been logged out. You will be redirected back to the start page";
+
 ?>
