e9820045b5d879e1c8289f4ba8e7f9fbe96d4b5b
Alexia <alexiabaxter07@gmail.com>
2024-10-14 13:53:06 +0100

    Update db_connect.php

--- Assignments/To Do list/db_connect.php
+++ Assignments/To Do list/db_connect.php
@@ -7,7 +7,6 @@ $dname = "to-do";
 try {
     $conn = new PDO("mysql:host=$servername;port =3306;dbname=$dname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo"connection success";
 }catch(PDOException $e){
     echo "conection failed". $e->getMessage();
 }
