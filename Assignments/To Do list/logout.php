d5a325470b9b94abd2113a99ca527e93cd7ec490
Alexia <alexiabaxter07@gmail.com>
2024-10-16 11:17:06 +0100

    updated: linked cs to log out

--- Assignments/To Do list/logout.php
+++ Assignments/To Do list/logout.php
@@ -3,7 +3,7 @@
     //session means that variables can be used across page
     session_start();
 
-    echo "<link rel='stylesheet' href='styles.css'>";
+
 
 
     // remove all session variables
@@ -12,8 +12,11 @@
     // destroy the session
     session_destroy();
 
-    echo "You have now been logged out. You will be redirected back to the start page";
-    header("refresh:1; url=index.html");
 
+    header("refresh:1; url=index.html");
+    echo "<link rel='stylesheet' href='styles.css'>";
+    echo "<div id='loggy'>";
+    echo "You have now been logged out. You will be redirected back to the start page";
+    echo "<\div>";
 
 ?>
