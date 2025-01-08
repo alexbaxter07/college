de45239ba1fd8bed39f672b1fb32a2c8864640a8
alexbaxter07 <alexbaxter07@gmail.com>
2025-01-08 14:39:49 +0000

    created verify login page

---
+++ projects/Hours_Tracker/login/verify_log.php
@@ -0,0 +1,33 @@
+<?php
+
+    include '../db_connect.php';
+
+    $position = $_POST['position'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $sql= 'SELECT * FROM ? WHERE email = ? AND password = ?';
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(1,$position);
+    $stmt->bind_param(2, $email);
+    $stmt->bind_param(3, $pass);
+    $stmt->execute();
+    $row = $stmt->fetch();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $rpswd = $result["Password"];
+
+    if (password_verify($pass, $rpswd)) {
+
+        header("refresh:1; url=../student/stu_homepage.html");
+        echo "you are now logged in! Heading to your homepage";
+
+    } else {
+
+        header("refresh:1; url=../login/login.html");
+        echo "Password is incorrect";
+
+    }
+?>
