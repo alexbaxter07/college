928ae4bfa0a1ac54017ecb87b4fb6d0a46ad54ab
alexbaxter07 <alexbaxter07@gmail.com>
2025-01-08 15:24:10 +0000

    changed from html to php so that a list can be shown on that page

---
+++ projects/Hours_Tracker/student/stu_homepage.php
@@ -0,0 +1,45 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Student Homepage</title>
+    <link href="../styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div id="container">
+
+    <div id="titlebar">
+        <h1>Hours Tracker</h1>
+    </div>
+
+    <div id="navbar">
+
+        <ul id="nav">
+            <li><a href="hours_left.php">Hours Left</a></li>
+            <li><a href="completed_hours.php">Completed hours</a></li>
+            <li><a href="company_contact.php">company contact</a></li>
+        </ul>
+
+    </div>
+
+    <div id="content">
+
+        <h3>Homepage</h3>
+
+
+
+
+    </div>
+
+</div>
+
+</body>
+
+</html>
