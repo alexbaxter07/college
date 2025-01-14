0bc3b90540f5550a40d4726751f628dbeb144607
Alexia <161457494+alexbaxter07@users.noreply.github.com>
2025-01-14 20:27:25 +0000

    created: start of completed hours

---
+++ projects/Hours_Tracker/student/completed_hours.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Student Homepage</title>
+    <link href="../styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div id="container">
+
+    <div id="titlebar">
+        <h1>Hours Tracker</h1>
+    </div>
+
+    <div id="navbar">
+
+        <ul id="nav">
+            <li><a href="hours_left.php">Hours Left</a></li>
+            <li><a href="completed_hours.php">Completed hours</a></li>
+            <li><a href="company_contact.php">company contact</a></li>
+        </ul>
+
+    </div>
