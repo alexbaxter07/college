2d42c388e381d55f261957265f50c5e5e4dbbfc6
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-04 11:54:41 +0000

    updated: database connect updated commenting

--- prep/may24/db_connect.php
+++ prep/may24/db_connect.php
@@ -5,6 +5,7 @@
     $password = "";
     $dname = "zoo";
 
+    //trys connecting to database if it cant then gives an error message for robustness
     try {
         $conn = new PDO("mysql:host=$servername;port =3306;dbname=$dname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
