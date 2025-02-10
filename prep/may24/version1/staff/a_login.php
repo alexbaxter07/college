2b0de37a7a77b4573f61efed6527a989acbad481
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-10 13:12:59 +0000

    updated: comment

--- prep/may24/version1/staff/a_login.php
+++ prep/may24/version1/staff/a_login.php
@@ -3,6 +3,6 @@
 
 include("db_connect.php");
 
-// here will be the information for the login but the database needs some data first so this will be added to later
+// here will be the information for the admin login but the database needs some data first so this will be added to later
 
 ?>
