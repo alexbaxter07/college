8ded97d3ca5028664948b4f718ec6a8d76c79d75
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-11 13:16:47 +0000

    updated: created an add admin page for super level admins only

---
+++ prep/may24/version1/staff/admin/admin_pages/add/add_admin.php
@@ -0,0 +1,104 @@
+<?php
+
+    include "../../a_functions.php";
+
+    session_start();
+
+    if($_SESSION["level"]!='SUPER'){
+
+        header("refresh:4; location: a_index.php");
+        echo "<link rel='stylesheet' type='text/css' href='../../../../styles.css'>";
+        echo "Admin already exists. login or ask to be registered.";
+    }else{
+
+        echo "<DOCTYPE html>";
+
+            echo "<head>";
+
+                echo "<link rel='stylesheet' type='text/css' href='../../../../styles.css'>";
+                echo"<title>Add Admin</title>";
+
+            echo "</head>";
+
+            echo "<body>";
+
+                include "../../a_navbar.php";
+
+                echo "<h2>Add Admin Form</h2>"; // Heading for the registration form
+
+                echo "<form action='add_admin_reg.php' method='post'>"; // Form for user registration
+
+                    echo "<table>"; // Table to organize form fields
+
+                        echo "<tr>";
+
+                            echo "<td><label for='uname'>Username:</label></td>";
+                            echo "<td><input type='text' id='uname' name='uname' placeholder='Enter a username' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='fname'>First Name:</label></td>";
+                            echo "<td><input type='text' id='fname' name='fname' placeholder='Enter your first name' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='sname'>Surname:</label></td>";
+                            echo "<td><input type='text' id='sname' name='sname' placeholder='Enter your surname' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='email'>Email:</label></td>";
+                            echo "<td><input type='email' id='email' name='email' placeholder='Enter your email' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='password'>Password:</label></td>";
+                            echo "<td><input type='password' id='password' name='password' placeholder='Enter your password' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='cpassword'>Confirm Password:</label></td>";
+                            echo "<td><input type='password' id='cpassword' name='cpassword' placeholder='Confirm your password' required></td>";
+
+                        echo "</tr>";
+
+                        echo "<tr>";
+
+                            echo "<td><label for='admin_type'>Select type of admin:</label></td>";
+
+                            echo "<td>";
+                                echo "<select id='admin_type' name='Aadmin_type'>";
+                                    echo "<option value='CREATOR'>Creator</option>";
+                                    echo "<option value='EDITOR'>Editor</option>";
+                                    echo "<option value='SUPER'>Super</option>";
+                                    echo "</select>";
+                            echo "</td>";
+                        echo"</tr>";
+
+                        echo "<tr>"; // Submit button for the form, spans two columns
+
+                            echo "<td colspan='2'><input type='submit' value='Register'></td>";
+
+                        echo "</tr>";
+
+                    echo "</table>"; // End of table for form fields
+
+                echo "</form>";
+
+            echo "</body>";
+
+        echo "</html>";
+
+    }
+
+?>
