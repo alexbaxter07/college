9b30be606ca0662ba01928fd3d65185a21c7f163
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-11 09:58:58 +0000

    updated: created login form for admin users

--- prep/may24/version1/staff/a_login.php
+++ prep/may24/version1/staff/a_login.php
@@ -1,8 +1,53 @@
 <?php
+//page to get information to login an admin
 
+session_start(); // start session to include session variables across pages it is used
 
-include("db_connect.php");
-
-// here will be the information for the admin login but the database needs some data first so this will be added to later
+include("../db_connect.php");
 
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <link rel="stylesheet" href="../styles.css">
+        <title>Admin Login</title>
+
+    </head>
+
+    <body>
+
+        <form id="admin_log_form" method="post" action="a_validate.php">  <!-- admin login form that will send the results to a validate form -->
+
+            <table id="admin_login"> <!-- table to organise form -->
+
+                <tr>
+                    <td><label for="uname">Username:</label></td>
+                    <td><input type="text" id="uname" name="uname" placeholder="Enter a username" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="email">Email:</label></td>
+                    <td><input type="email" id="email" name="email" placeholder="Enter your email" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="password">Password:</label></td>
+                    <td><input type="password" id="password" name="password" placeholder="Enter your password" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="cpassword">Confirm Password:</label></td>
+                    <td><input type="password" id="cpassword" name="cpassword" placeholder="Confirm your password" required></td>
+                </tr>
+
+            </table>
+
+        </form>
+
+    </body>
+
+</html>
