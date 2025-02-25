14182fd08cb3a98b1deb89633c1053873e333228
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-25 14:29:22 +0000

    updated: added homepage

--- prep/may24/version 2/consumer/index.php
+++ prep/may24/version 2/consumer/index.php
@@ -1,1 +1,23 @@
 <?php
+//homepage for users
+
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+
+    <html lang="en">
+
+    <head>
+
+        <link rel="stylesheet" href="../styles.css">
+        <title>User Homepage</title>
+
+    </head>
+
+    <body>
+
+        <--!content here-->
+
+    </body>
