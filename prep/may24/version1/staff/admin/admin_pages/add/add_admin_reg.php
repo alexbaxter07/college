a088f76f2bb858e86c605d2be550a68c032478ac
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-25 10:21:29 +0000

    updated: completed admin registration to insert data into the database

--- prep/may24/version1/staff/admin/admin_pages/add/add_admin_reg.php
+++ prep/may24/version1/staff/admin/admin_pages/add/add_admin_reg.php
@@ -23,6 +23,29 @@
 
         try{
 
+            //try this code
+
+            $sql = "INSERT INTO admin_user (username, first_name,last_name, email, password, signup_date, privliledge) VALUES (?, ?, ?, ?, ?, ?, ?)";  //prepare the sql to be sent
+            $stmt = $conn->prepare($sql); //prepare to sql
+
+            $stmt->bindParam(1,$_POST['uname']);  //bind parameters for security
+            $hpswd = password_hash($_POST['password'], PASSWORD_DEFAULT);  //hash the password
+            $stmt->bindParam(2,$hpswd);
+            $stmt->bindParam(3,$_POST['email']);
+            $stmt->bindParam(4,$_POST['fname']);
+            $stmt->bindParam(5,$_POST['sname']);
+            $signup_date = time();
+            $stmt->bindParam(6,$signup_date);
+            $stmt->bindParam(7,$_POST['admin_type']);
+
+            $stmt->execute();  //run the query to insert
+            $admin_reg_type = strtolower($_POST['admin_type']) . "reg";
+            $admin_reg_task = "Registration of a " . strtolower($_POST['admin_type']) . " admin user";
+            auditor($_POST['username'], $admin_reg_type, $admin_reg_task);
+
+            header("refresh:5; url=admin_login.php"); //confirm and redirect
+            echo "<link rel='stylesheet' href='../../../../styles.css'>";
+            echo "Successfully registered";
 
         } catch (PDOException $e) {
 
