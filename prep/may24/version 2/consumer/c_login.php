0659c83067a8cc0b7444aa49302904cc9bf17e99
alexbaxter07 <alexbaxter07@gmail.com>
2025-02-25 14:27:39 +0000

    updated: made login and validaition of user login

--- prep/may24/version 2/consumer/c_login.php
+++ prep/may24/version 2/consumer/c_login.php
@@ -1,7 +1,65 @@
 <?php
 
-    include("db_connect.php");
+    include("../db_connect.php");
 
-// here will be the information for the login but the database needs some data first so this will be added to later
+
+    //page to get information to log in a user
+
+    session_start(); // start session to include session variables across pages it is used
+
+    include("../functions.php");
+
+    if (user_sesh_started()) {
+        header("location: index.php");
+    }
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <link rel="stylesheet" href="../styles.css">
+        <title>Admin Login</title>
+
+    </head>
+
+    <body>
+
+        <form id="user_log_form" method="post" action="c_reg.php">
+            <!-- user login form that will send the results to a validate form -->
+
+            <table id="admin_login"> <!-- table to organise form -->
+
+                <tr>
+                    <td><label for="uname">Username:</label></td>
+                    <td><input type="text" id="uname" name="uname" placeholder="Enter a username" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="email">Email:</label></td>
+                    <td><input type="email" id="email" name="email" placeholder="Enter your email" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="password">Password:</label></td>
+                    <td><input type="password" id="password" name="password" placeholder="Enter your password" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="cpassword">Confirm Password:</label></td>
+                    <td><input type="password" id="cpassword" name="cpassword" placeholder="Confirm your password" required>
+                    </td>
+                </tr>
+
+            </table>
+
+        </form>
+
+    </body>
+
+</html>
 
 ?>
--- prep/may24/version 2/consumer/c_reg.php
+++ prep/may24/version 2/consumer/c_reg.php
@@ -1,40 +1,59 @@
 <?php
 
-    // Start session to use session variables across pages
-    session_start();
+// page to validate the information from user login
 
-    //includes the database connection so that we can get information into the database
-    include("../db_connect.php");
+    try{ // try except to catch any errors
 
-    $username = $_POST['uname'];
-    $password = $_POST['password'];
-    $c_password = $_POST['cpassword'];
-    $email = $_POST['email'];
-    $f_name = $_POST['fname'];
-    $l_name = $_POST['sname'];
+        session_start(); //session start so that the page can connect to session data
+        //includes neccassary functions needed
+        include("../functions.php");
 
-    //check if inputted passwords by user match
-    if ($password != $c_password) {
+        $username = $_POST['uname'];
 
-        //redirects to signup as passwords do not mach
-        header("Location: c_signup.php");
-        echo "Passwords do not match";
-
-    }else{
-
-
-
-
-        $sql = "Insert into user(user_name, first_name, last_name, password, user_type_id, signup_date) values (?,?,?,?,?,?,?)";
-
+        $sql = "SELECT user_name, email FROM user WHERE user_name = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam(1, $username);
-        $stmt->bindParam(2, $f_name);
-        $stmt->bindParam(3, $l_name);
-        $stmt->bindParam(4, $password);
-        $stmt->bindParam(5, $email);
-        
+        $stmt -> bindParam(1, $username);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($result){ // if user existes with given details
+
+            if(password_verify($_POST['password'], $result['password'])){ // if password match database
+
+                //creation of session variables
+                $_SESSION["user_login"] = true;
+                $_SESSION["username"] = $_POST['uname'];
+
+                // send to admin index
+                header("location: index.php");
+
+                exit();
+
+            }else{
+
+                session_destroy(); // if failed kills session
+
+                header("refresh:4; location: a_login.php"); //send back to login
+
+                echo "<link rel='stylesheet' href='../styles.css' type='css'>"; // link sylesheet
+
+                echo "Wrong password"; //error message
+
+            }
+
+        }else{
+
+            header("refresh:4; location: c_login.php"); // send back to login
+
+            echo "<link rel='stylesheet' href='../styles.css' type='css'>"; //link to stylesheet
+
+            echo "User not found"; // error message
+
+        }
+
+    }catch(Exception $e){ //gets error
+
+        echo $e; // outputs error message
 
     }
-
 ?>
